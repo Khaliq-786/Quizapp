8f6c1deaf6dbee82db526efb576614d5193b57ef
Khaliq Hussain <khaliqrocks786@gmail.com>
2022-11-01 04:05:04 +0530

    Adding Firebase to app

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/services/auth.dart';
 import 'package:quizapp/shared/bottom_nav.dart';
+import 'package:quizapp/topics/topics.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Khana dedo'),
-          onPressed: () => Navigator.pushNamed(context, '/about'),
-        ),
-      ),
-      bottomNavigationBar: BottomNavBar(),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading');
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('error'),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
